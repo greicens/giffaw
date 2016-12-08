a4749d03c34569bb32505c673ffb09c8d3b5fb99
greicens <gsilva@mail.ccsf.edu>
2016-12-08 00:10:02 -0800

    final touches

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,32 @@
+console.log("JS is working")
 $(document).on("ready", function(){
+  $('form').on("submit", searchReq);
 
 });
+
+function searchReq(event){
+  console.log("searchReq function");
+  event.preventDefault();
+  $.ajax({
+    method: "GET",
+    url: "http://api.giphy.com/v1/gifs/search",
+    dataType: "json",
+    data: $("form").serialize(),
+    success: successCallback,
+    error: errorCallback,
+  })
+}
+
+function successCallback(response){
+  //this forEach loop is going through all the data on the response object
+   response.data.forEach(function(element,i){
+     if(i < 25){
+       $(".gif-gallery").append($("<img src="+element.images.fixed_height.url+">"));
+     }
+   });
+
+}
+
+function errorCallback(){
+  console.log("error");
+}
